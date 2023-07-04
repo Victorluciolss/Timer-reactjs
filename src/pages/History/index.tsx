e782fb2a8accb55c8d642d6c524c65dd17d1a855
victor <victorluciosilvasilvano@gmail.com>
2023-07-04 17:59:08 +0100

    Home and history pages

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,58 @@
+import { HistoryContainer, HistoryList, Status } from './styled'
+
+export function History() {
+  return (
+    <div>
+      <HistoryContainer>
+        <h1>Meu Histórico</h1>
+
+        <HistoryList>
+          <table>
+            <thead>
+              <tr>
+                <th>Tarefa</th>
+                <th>Duração</th>
+                <th>Início</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Tarefa</td>
+                <td>20 minutos</td>
+                <td>Há 2 meses</td>
+                <td>
+                  <Status>Concluído</Status>
+                </td>
+              </tr>
+              <tr>
+                <td>Tarefa</td>
+                <td>20 minutos</td>
+                <td>Há 2 meses</td>
+                <td>
+                  <Status>Concluído</Status>
+                </td>
+              </tr>
+              <tr>
+                <td>Tarefa</td>
+                <td>20 minutos</td>
+                <td>Há 2 meses</td>
+                <td>
+                  <Status>Concluído</Status>
+                </td>
+              </tr>
+              <tr>
+                <td>Tarefa</td>
+                <td>20 minutos</td>
+                <td>Há 2 meses</td>
+                <td>
+                  <Status>Concluído</Status>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </HistoryList>
+      </HistoryContainer>
+    </div>
+  )
+}
